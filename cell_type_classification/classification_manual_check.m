175cb4261d86edbfe6a828b79149d87ef4ff81c0
pauline-kerekes <pauline.kerekes@gmail.com>
2026-06-18 11:50:30 +0100

    batch check classification

---
+++ cell_type_classification/classification_manual_check.m
@@ -0,0 +1,10 @@
+
+function classification_manual_check(folder_session)
+
+    % get the clusters indices and the corresponding cell types
+    load(strcat(folder_session,'\cell_type_classification\classification_scores.mat'));
+    
+    
+    %
+
+end
---
+++ cell_type_classification/classification_manual_check_batch.m
@@ -0,0 +1,4 @@
+
+
+folder_session = 'W:\mEC_tau_ephys\MH503\030824\concatenated_file';
+classification_manual_check(folder_session);
